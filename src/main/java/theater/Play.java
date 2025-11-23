08e879b5699f6fbaf817b1fa8f050c77d291e747
Aaron <jialun.wei@mail.utoronto.ca>
2025-11-23 16:44:14 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,23 @@
 package theater;
 
+/**
+ * This is the class storing info about a play.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
